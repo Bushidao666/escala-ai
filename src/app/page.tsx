b8efb9cfb72ab691889238c410e5df3523305f5e
Bushidao666 <jprb.siqueira22@hotmail.com>
2025-06-23 02:30:42 -0300

    Indexacao da Pagina de Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-export default function GalleryPage() {
-  return (
-    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Galeria</h1>
-      </div>
-      <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-      >
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            Nenhum criativo gerado ainda
-          </h3>
-          <p className="text-sm text-muted-foreground">
-            Comece criando um novo criativo para vê-lo aqui.
-          </p>
-        </div>
-      </div>
-    </main>
-  );
+import LoginPage from "@/app/auth/login/page";
+
+export default function HomePage() {
+  return <LoginPage />;
 }
